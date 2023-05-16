a31bea553fc980bf0557d1e2ca975b9cd774dfe8
djsiso <djsisodiya@gmail.com>
2023-05-17 02:07:40 +0530

    General Python
    Adding two scripts

---
+++ General_Python/remove_duplicates.py
@@ -0,0 +1,28 @@
+# Remove the duplicates from given list
+# s = [1,2,4,2,1,4,7]
+def remove_duplicates_from_int_list(input_list):
+    # Case-1
+    # Input list is integer
+    # Time Complexity is O(n) where n is the length of input list
+    return [*set(input_list)]
+
+# s1 = ['D','I','G','V','I','J','A','Y']
+# s1 = "Digvijay"
+def remove_duplicates_from_str_list(input_list):
+    # Case-2
+    # input list is string
+    # Time Complexity is O(n * m), where n is the length of s1 and m is the number of unique elements in s1.
+    final_list=[]
+    for i in input_list:
+        if i not in final_list:
+            final_list.append(i)
+    return [*final_list] # D i g v j a y
+
+s = [1,2,4,2,1,4,7]
+final_list = remove_duplicates_from_int_list(s)
+# s1 = ['D','I','G','V','I','J','A','Y']
+s1 = "Digvijay"
+str_final_list = remove_duplicates_from_str_list(s1)
+
+print(final_list)
+print(str_final_list)
---
+++ General_Python/second_largest_number.py
@@ -0,0 +1,16 @@
+# Find second largest number in an input
+def second_largest_number(input_list):
+    # Time Complexity: O(n), where n is the length of the input list.
+    largest = None
+    second_largest = None
+    for i in input_list:
+        if largest is None or i > largest:
+            second_largest = largest
+            largest = i
+        elif second_largest is None or i > second_largest:
+            second_largest = i
+    return second_largest
+    
+n = [1,2,4,2,1,4,7]
+numbers = second_largest_number(n)
+print(numbers)
