cd323db26bbce81a27e9ff621897ba9a949cca69
djsiso <djsisodiya@gmail.com>
2023-05-17 02:22:48 +0530

    updated code

--- General_Python/remove_duplicates.py
+++ General_Python/remove_duplicates.py
@@ -1,24 +1,29 @@
-# Remove the duplicates from given list
+"""
+- Remove the duplicates from given list.
+- Case-1: Input list is integer
+- Case-2 : Input list is string
+"""
 def remove_duplicates_from_int_list(input_list):
-    # Case-1
-    # Input list is integer
-    # Time Complexity is O(n) where n is the length of input list
+    """
+    - Time Complexity is O(n) where n is the length of input list
+    """
     return [*set(input_list)]
 
 def remove_duplicates_from_str_list(input_list):
-    # Case-2
-    # input list is string
-    # Time Complexity is O(n * m), where n is the length of s1 and m is the number of unique elements in s1.
+    """
+    - Time Complexity is O(n * m)
+    - Where n is the length of s1 and m is the number of unique elements in s1.
+    """
     final_list=[]
     for i in input_list:
         if i not in final_list:
             final_list.append(i)
     return [*final_list] # D i g v j a y
 
-s = [1,2,4,2,1,4,7]
-final_list = remove_duplicates_from_int_list(s)
+input_1 = [1,2,4,2,1,4,7]
+final_list = remove_duplicates_from_int_list(input_1)
 # s1 = ['D','I','G','V','I','J','A','Y']
-s1 = "Digvijay"
-str_final_list = remove_duplicates_from_str_list(s1)
+input_2 = "Digvijay"
+str_final_list = remove_duplicates_from_str_list(input_2)
 print(final_list)
 print(str_final_list)
