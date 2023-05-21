455e7acb1c37f94db7293f616f35debd6cf7d2e2
djsiso <djsisodiya@gmail.com>
2023-05-22 01:08:32 +0530

    code updated

--- General_Python/matrix_multiplication.py
+++ General_Python/matrix_multiplication.py
@@ -9,29 +9,14 @@ def matrix_multiply(matrix1, matrix2):
     cols1 = len(matrix1[0])
     rows2 = len(matrix2)
     cols2 = len(matrix2[0])
-
     if cols1 != rows2:
         raise ValueError("Number of columns in matrix1 must be equal to the number of rows in matrix2.")
-
     result = [[0 for _ in range(cols2)] for _ in range(rows1)]
-
     for i in range(rows1):
         for j in range(cols2):
             for k in range(cols1):
                 result[i][j] += matrix1[i][k] * matrix2[k][j]
-
     return result
 
 def matrix_multiply_using_numpy():
     return np.dot(matrix1, matrix2)
-
-
-# matrix1 = [[1, 2, 3],
-#            [4, 5, 6]]
-# matrix2 = [[7, 8],
-#            [9, 10],
-#            [11, 12]]
-# result = matrix_multiply(matrix1, matrix2)
-# print(result)
-# res = matrix_multiply_using_numpy(matrix1, matrix2)
-# print(result)
